658d81153b7316388a0ca49fe02ecf746fe7dc2d
useratnns <musmanboota100@gmail.com>
2026-04-21 08:44:04 +0500

    Initial commit

---
+++ app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantAdapter.kt
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.samples.apps.sunflower.PlantListFragment
+import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.HomeViewPagerFragmentDirections
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.databinding.ListItemPlantBinding
+import java.security.InvalidParameterException
+
+/**
+ * Adapter for the [RecyclerView] in [PlantListFragment].
+ */
+class PlantAdapter : ListAdapter<Plant, RecyclerView.ViewHolder>(PlantDiffCallback()) {
+    /**
+     * Int array of all the positions with header-style cards
+     */
+    private val headerPositions = intArrayOf(0)
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (getItemViewType(position)) {
+            VIEW_TYPE_HEADER -> {
+                // cast generic RecyclerView.ViewHolder to header view holder
+                val holderViewParams = (holder as HeaderViewHolder).itemView.layoutParams
+                val layoutParams = holderViewParams as StaggeredGridLayoutManager.LayoutParams
+
+                // set width of header to span entire screen
+                layoutParams.isFullSpan = true
+            }
+            VIEW_TYPE_PLANT -> {
+                val plant = getItem(position)
+
+                // cast generic RecyclerView.ViewHolder to Plant view holder
+                (holder as PlantViewHolder).bind(plant)
+            }
+            else -> throw InvalidParameterException()
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+
+        // if current position is where a header should go, return header view type
+        return when (position) {
+            in headerPositions -> R.layout.plant_list_header
+            else -> R.layout.list_item_plant
+        }
+    }
+
+    override fun getItemCount(): Int {
+
+        // If no available plants, don't print header either, otherwise return number of cards needed
+        return if (currentList.isEmpty()) {
+            0
+        } else {
+            currentList.size + headerPositions.size
+        }
+    }
+
+    override fun getItem(position: Int): Plant {
+
+        // index in plant list is position subtracted by offset of number of headers
+        return currentList[position - headerPositions.size]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        // Return a specific viewholder type depending on current viewType
+        return when (viewType) {
+
+            // Generic layout inflate from Layout XML file
+            VIEW_TYPE_HEADER -> HeaderViewHolder(LayoutInflater.from(parent.context)
+                    .inflate(R.layout.plant_list_header, parent, false))
+
+            VIEW_TYPE_PLANT -> PlantViewHolder(ListItemPlantBinding
+                    .inflate(LayoutInflater.from(parent.context), parent, false))
+
+            else -> throw InvalidParameterException("Invalid viewType")
+        }
+    }
+
+    class PlantViewHolder(
+        private val binding: ListItemPlantBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.setClickListener {
+                binding.plant?.let { plant ->
+                    navigateToPlant(plant, it)
+                }
+            }
+        }
+
+        private fun navigateToPlant(
+            plant: Plant,
+            it: View
+        ) {
+            val direction = HomeViewPagerFragmentDirections.actionViewPagerFragmentToPlantDetailFragment(plant.plantId)
+            it.findNavController().navigate(direction)
+        }
+
+        fun bind(item: Plant) {
+            binding.apply {
+                plant = item
+                executePendingBindings()
+            }
+        }
+    }
+
+    /**
+     * HeaderViewHolder class is a basic implementation of RecyclerView.ViewHolder
+     * Adapt this class if custom bindings are necessary
+     */
+    class HeaderViewHolder(
+        headerView: View
+    ) : RecyclerView.ViewHolder(headerView)
+
+    companion object {
+        private const val VIEW_TYPE_HEADER = R.layout.plant_list_header
+        private const val VIEW_TYPE_PLANT = R.layout.list_item_plant
+    }
+}
+
+private class PlantDiffCallback : DiffUtil.ItemCallback<Plant>() {
+
+    override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem.plantId == newItem.plantId
+    }
+
+    override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem == newItem
+    }
+}
